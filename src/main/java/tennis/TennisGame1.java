94c0b092328c9aebaf96dbf9f98b9dabf152936a
Antoine MICHAUD <antoinemichaud34@gmail.com>
2015-04-28 22:30:37 +0200

    Remove some warnings

--- src/main/java/tennis/TennisGame1.java
+++ src/main/java/tennis/TennisGame1.java
@@ -4,16 +4,12 @@ public class TennisGame1 implements TennisGame {
     
     private int m_score1 = 0;
     private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
 
     public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName.equals("player1"))
             m_score1 += 1;
         else
             m_score2 += 1;
@@ -21,7 +17,7 @@ public class TennisGame1 implements TennisGame {
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
+        int tempScore;
         if (m_score1==m_score2) {
             switch (m_score1) {
                 case 0:

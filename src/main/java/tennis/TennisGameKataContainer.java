cff7cae207f8c9df76814ba82d3de35c40382e47
Antoine MICHAUD <antoinemichaud34@gmail.com>
2015-05-19 13:30:06 +0200

    Rename util method

--- src/main/java/tennis/TennisGameKataContainer.java
+++ src/main/java/tennis/TennisGameKataContainer.java
@@ -2,13 +2,13 @@ package tennis;
 
 public class TennisGameKataContainer {
 
-
     public String displayScore(String player1, int player1score, String player2, int player2score) {
         TennisGame tennisGame = new TennisGame(player1, player2);
-        return checkAllScores(tennisGame, player1, player1score, player2, player2score);
+        initScores(tennisGame, player1, player1score, player2, player2score);
+        return getScore(tennisGame);
     }
 
-    public String checkAllScores(TennisGame game, String player1Name, int player1Score, String player2Name, int player2Score) {
+    public void initScores(TennisGame game, String player1Name, int player1Score, String player2Name, int player2Score) {
         int highestScore = Math.max(player1Score, player2Score);
         for (int i = 0; i < highestScore; i++) {
             if (i < player1Score)
@@ -16,6 +16,9 @@ public class TennisGameKataContainer {
             if (i < player2Score)
                 game.wonPoint(player2Name);
         }
+    }
+
+    private String getScore(TennisGame game) {
         return game.getScore();
     }
 }
--- src/test/java/tennis/TennisGameKataContainerTest.java
+++ src/test/java/tennis/TennisGameKataContainerTest.java
@@ -1,10 +1,9 @@
 package tennis;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TennisGameKataContainerTest {
 

9a3f25e268561da1a45b65bb07a6540f93e5a19e
Antoine MICHAUD <antoinemichaud34@gmail.com>
2015-06-03 21:28:47 +0200

    Add delegation to TennisSet from TennisSetKataContainer

---
+++ src/main/java/tennis/set/TennisSet.java
@@ -0,0 +1,15 @@
+package tennis.set;
+
+public class TennisSet {
+    public void player1WonPoint() {
+
+    }
+
+    public void player2WonPoint() {
+
+    }
+
+    public String score() {
+        return null;
+    }
+}
--- src/main/java/tennis/set/TennisSetKataContainer.java
+++ src/main/java/tennis/set/TennisSetKataContainer.java
@@ -4,7 +4,19 @@ import java.util.List;
 
 public class TennisSetKataContainer {
 
+    TennisSet tennisSet = new TennisSet();
+
     public String displayScore(List<Integer> player1Scores, List<Integer> player2Scores) {
-        return null;
+        for (int i = 0; i < player1Scores.size(); i++) {
+            Integer player1Score = player1Scores.get(i);
+            Integer player2Score = player2Scores.get(i);
+            for (int j = 0; j < Math.max(player1Score, player2Score); j++) {
+                if (i <= player1Score)
+                    tennisSet.player1WonPoint();
+                if (i <= player2Score)
+                    tennisSet.player2WonPoint();
+            }
+        }
+        return tennisSet.score();
     }
 }

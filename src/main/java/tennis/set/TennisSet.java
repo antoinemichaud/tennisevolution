cb434d991f7fa1825fd4e3766587c5e1992e6629
Antoine MICHAUD <antoinemichaud34@gmail.com>
2015-06-04 13:20:20 +0200

    Factor points winning

--- src/main/java/tennis/set/TennisSet.java
+++ src/main/java/tennis/set/TennisSet.java
@@ -3,33 +3,35 @@ package tennis.set;
 import tennis.game.EnglishGameDisplayer;
 import tennis.game.TennisGameDisplayer;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class TennisSet {
 
     public static final String PLAYER_1 = "player1";
     public static final String PLAYER_2 = "player2";
 
     TennisGameDisplayer tennisGameDisplayer = new EnglishGameDisplayer(PLAYER_1, PLAYER_2);
-    private int player1wonGames;
-    private int player2wonGames;
+    private AtomicInteger player1wonGames;
+    private AtomicInteger player2wonGames;
 
     public TennisSet() {
-        player1wonGames = 0;
-        player2wonGames = 0;
+        player1wonGames = new AtomicInteger(0);
+        player2wonGames = new AtomicInteger(0);
     }
 
     public void player1WonPoint() {
-        tennisGameDisplayer.wonPoint(PLAYER_1);
-        if (tennisGameDisplayer.getScore().equals("Win for player1")) {
-            tennisGameDisplayer = new EnglishGameDisplayer(PLAYER_1, PLAYER_2);
-            player1wonGames++;
-        }
+        playerWonPoint(PLAYER_1, player1wonGames);
     }
 
     public void player2WonPoint() {
-        tennisGameDisplayer.wonPoint(PLAYER_2);
-        if (tennisGameDisplayer.getScore().equals("Win for player2")) {
+        playerWonPoint(PLAYER_2, player2wonGames);
+    }
+
+    private void playerWonPoint(String playerName, AtomicInteger pointsToIncrease) {
+        tennisGameDisplayer.wonPoint(playerName);
+        if (tennisGameDisplayer.getScore().equals("Win for " + playerName)) {
             tennisGameDisplayer = new EnglishGameDisplayer(PLAYER_1, PLAYER_2);
-            player2wonGames++;
+            pointsToIncrease.incrementAndGet();
         }
     }
 

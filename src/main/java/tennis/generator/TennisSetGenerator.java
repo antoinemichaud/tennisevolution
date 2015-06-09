3c6e2657a74dc58073aa1c1abb158652069013ab
Antoine MICHAUD <antoinemichaud34@gmail.com>
2015-06-09 13:01:14 +0200

    Generation of a set

--- src/main/java/tennis/generator/TennisSetGenerator.java
+++ src/main/java/tennis/generator/TennisSetGenerator.java
@@ -2,19 +2,22 @@ package tennis.generator;
 
 import tennis.game.TennisGame;
 import tennis.history.HistoryKeeper;
+import tennis.set.TennisSet;
 
 import java.util.Random;
 import java.util.function.Predicate;
 
 public class TennisSetGenerator {
     private Random random;
+    private TennisSet tennisSet;
+    private TennisGame tennisGame;
 
     public TennisSetGenerator(Random random) {
         this.random = random;
     }
 
     public HistoryKeeper generate(Predicate<TennisGame> endCondition) {
-        TennisGame tennisGame = new TennisGame();
+        tennisGame = new TennisGame();
         HistoryKeeper historyKeeper = new HistoryKeeper(tennisGame);
 
         do {
@@ -25,5 +28,23 @@ public class TennisSetGenerator {
             }
         } while (!endCondition.test(tennisGame));
         return historyKeeper;
+    }
+
+    public HistoryKeeper generateSet(Predicate<TennisSet> endCondition) {
+        tennisSet = new TennisSet();
+        HistoryKeeper historyKeeper = new HistoryKeeper(tennisSet);
+
+        do {
+            if (Math.round(random.nextDouble()) == 0) {
+                tennisSet.player1WonPoint();
+            } else {
+                tennisSet.player2WonPoint();
+            }
+        } while (!endCondition.test(tennisSet));
+        return historyKeeper;
+    }
+
+    public String tennisSetScore() {
+        return tennisSet.score();
     }
 }
--- src/test/java/tennis/generator/TennisSetGeneratorTest.java
+++ src/test/java/tennis/generator/TennisSetGeneratorTest.java
@@ -28,6 +28,40 @@ public class TennisSetGeneratorTest {
     }
 
     @Test
+    public void should_generate_a_list_with_four_0() throws Exception {
+        // Given
+        TennisSetGenerator tennisSetGenerator = new TennisSetGenerator(new Random() {
+            @Override
+            public double nextDouble() {
+                return 0;
+            }
+        });
+
+        // When
+        HistoryKeeper tennisGameHistory = tennisSetGenerator.generateSet(tennisSet -> tennisSet.score().equals("1-0 Love-All"));
+
+        // Then
+        assertThat(tennisGameHistory.list()).containsExactly(0, 0, 0, 0);
+    }
+
+    @Test
+    public void should_generate_a_list_with_five_0() throws Exception {
+        // Given
+        TennisSetGenerator tennisSetGenerator = new TennisSetGenerator(new Random() {
+            @Override
+            public double nextDouble() {
+                return 0;
+            }
+        });
+
+        // When
+        HistoryKeeper tennisGameHistory = tennisSetGenerator.generateSet(tennisSet -> tennisSet.score().equals("1-0 Fifteen-Love"));
+
+        // Then
+        assertThat(tennisGameHistory.list()).containsExactly(0, 0, 0, 0, 0);
+    }
+
+    @Test
     public void should_generate_a_list_with_one_1() throws Exception {
         // Given
         TennisSetGenerator tennisSetGenerator = new TennisSetGenerator(new Random() {
@@ -63,16 +97,15 @@ public class TennisSetGeneratorTest {
     }
 
     @Test
-    public void generate_a_game() throws Exception {
+    public void generate_a_set() throws Exception {
         // Given
         TennisSetGenerator tennisSetGenerator = new TennisSetGenerator(new Random());
 
         // When
-        HistoryKeeper tennisGameHistory = tennisSetGenerator.generate(tennisGame ->
-                tennisGame.getScore().firstPlayerScore() == PlayerScore.GAME ||
-                        tennisGame.getScore().secondPlayerScore() == PlayerScore.GAME);
+        HistoryKeeper tennisHistory = tennisSetGenerator.generateSet(tennisSet -> tennisSet.score().contains("Set for"));
 
         // Then
-        System.out.println(tennisGameHistory.list());
+        System.out.println(tennisSetGenerator.tennisSetScore());
+        System.out.println(tennisHistory.list());
     }
 }

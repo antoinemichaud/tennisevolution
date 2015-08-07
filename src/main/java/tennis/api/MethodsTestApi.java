3d83da9346529134758a79b1944444260e947491
Antoine MICHAUD <antoinemichaud34@gmail.com>
2015-08-07 15:59:17 +0200

    API to sets scores

--- src/main/java/tennis/api/MethodsTestApi.java
+++ src/main/java/tennis/api/MethodsTestApi.java
@@ -1,12 +1,19 @@
 package tennis.api;
 
+import com.google.common.base.Splitter;
 import net.codestory.http.WebServer;
 import tennis.game.TennisGameKataContainer;
+import tennis.set.TennisSetKataContainer;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 public class MethodsTestApi {
 
     public static void main(String[] args) {
         TennisGameKataContainer tennisGameKataContainer = new TennisGameKataContainer();
+        TennisSetKataContainer tennisSetKataContainer = new TennisSetKataContainer();
 
         new WebServer().configure(
                 routes -> routes
@@ -25,7 +32,15 @@ public class MethodsTestApi {
                                         context.query().getInteger("player1Score"),
                                         context.get("player2Name"),
                                         context.query().getInteger("player2Score")))
-        ).start();
+                        .get("/sets/displayScore", (context) -> {
+                            String player1ScoresAsString = context.get("player1Scores");
+                            String player2ScoresAsString = context.get("player2Scores");
 
+                            List<Integer> player1Scores = Splitter.on(",").splitToList(player1ScoresAsString).stream().map(Integer::parseInt).collect(toList());
+                            List<Integer> player2Scores = Splitter.on(",").splitToList(player2ScoresAsString).stream().map(Integer::parseInt).collect(toList());
+
+                            return tennisSetKataContainer.displayScore(player1Scores, player2Scores);
+                        })
+        ).start();
     }
 }

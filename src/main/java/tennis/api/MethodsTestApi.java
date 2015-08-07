ccf5b8a311f1eadd25b25b601be4a5ff8ba8c267
Antoine MICHAUD <antoinemichaud34@gmail.com>
2015-08-07 10:48:05 +0200

    Add routes tennis game

--- src/main/java/tennis/api/MethodsTestApi.java
+++ src/main/java/tennis/api/MethodsTestApi.java
@@ -1,11 +1,31 @@
 package tennis.api;
 
 import net.codestory.http.WebServer;
+import tennis.game.TennisGameKataContainer;
 
 public class MethodsTestApi {
 
     public static void main(String[] args) {
-        new WebServer().configure(routes -> routes.get("/", "Hello World")).start();
-    }
+        TennisGameKataContainer tennisGameKataContainer = new TennisGameKataContainer();
 
+        new WebServer().configure(
+                routes -> routes
+                        .get("/displayScore", (context) -> tennisGameKataContainer
+                                .displayScore(context.get("player1Name"),
+                                        context.query().getInteger("player1Score"),
+                                        context.get("player2Name"),
+                                        context.query().getInteger("player2Score")))
+                        .get("/displayAlternativeScore", (context) -> tennisGameKataContainer
+                                .displayAlternativeScore(context.get("player1Name"),
+                                        context.query().getInteger("player1Score"),
+                                        context.get("player2Name"),
+                                        context.query().getInteger("player2Score")))
+                        .get("/displayFrenchScore", (context) -> tennisGameKataContainer
+                                .displayFrenchScore(context.get("player1Name"),
+                                        context.query().getInteger("player1Score"),
+                                        context.get("player2Name"),
+                                        context.query().getInteger("player2Score")))
+        ).start();
+
+    }
 }

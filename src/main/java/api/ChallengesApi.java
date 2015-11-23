f0c90f747aa8e6e2186ec469a7cb1ae573975ac8
Antoine MICHAUD <antoinemichaud34@gmail.com>
2015-11-23 13:39:45 +0100

    create route for step2, plugged to TennisAlternativeService

--- src/main/java/api/ChallengesApi.java
+++ src/main/java/api/ChallengesApi.java
@@ -1,18 +1,25 @@
 package api;
 
 import net.codestory.http.WebServer;
+import tennis.TennisAlternativeService;
 import tennis.TennisService;
 
 public class ChallengesApi {
 
     public static void main(String[] args) {
         TennisService tennisService = new TennisService();
+        TennisAlternativeService tennisAlternativeService = new TennisAlternativeService();
 
         new WebServer().configure(
                 routes -> routes
                         .get("/displayScore", (context) -> tennisService
                                 .displayScore(
                                         context.query().getInteger("player1Score"),
+                                        context.query().getInteger("player2Score")))
+                        .get("/displayAlternativeScore", (context) -> tennisAlternativeService
+                                .displayAlternativeScore(context.get("player1Name"),
+                                        context.query().getInteger("player1Score"),
+                                        context.get("player2Name"),
                                         context.query().getInteger("player2Score")))
         ).start(8080);
     }
--- src/main/java/tennis/TennisService.java
+++ src/main/java/tennis/TennisService.java
@@ -59,5 +59,4 @@ public class TennisService {
                 return "Forty";
         }
     }
-
 }

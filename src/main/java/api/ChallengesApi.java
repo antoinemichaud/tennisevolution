0119cfde47bb977ea97696346c0eb708f52c8e47
Antoine MICHAUD <antoinemichaud34@gmail.com>
2015-12-03 14:08:16 +0100

    adapt signature to implementation target

--- src/main/java/api/ChallengesApi.java
+++ src/main/java/api/ChallengesApi.java
@@ -30,6 +30,11 @@ public class ChallengesApi {
                                         context.query().getInteger("player1Score"),
                                         context.get("player2Name"),
                                         context.query().getInteger("player2Score")))
+                        .get("/withLifeScoring", (context) -> tennisService
+                                .displayScore(context.get("player1Name"),
+                                        context.query().getInteger("player1Score"),
+                                        context.get("player2Name"),
+                                        context.query().getInteger("player2Score")))
 
         ).start(8080);
     }

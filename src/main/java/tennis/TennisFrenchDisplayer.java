d5bba36af4a3bc2fb5e244411864347ed4e4fa39
Antoine MICHAUD <antoinemichaud34@gmail.com>
2015-12-06 17:36:42 +0100

    Refactor FrenchDisplayer

--- src/main/java/tennis/TennisFrenchDisplayer.java
+++ src/main/java/tennis/TennisFrenchDisplayer.java
@@ -2,37 +2,44 @@ package tennis;
 
 public class TennisFrenchDisplayer {
 
+    public static final String LOVE_ALL = "Zéro partout";
+    public static final String FIFTEEN_ALL = "Quinze partout";
+    public static final String THIRTY_ALL = "Trente partout";
+    public static final String DEUCE = "Egalité";
+    public static final String ADVANTAGE = "Avantage ";
+    public static final String WIN_FOR = "Jeu ";
+    public static final String LOVE = "Zéro";
+    public static final String FIFTEEN = "Quinze";
+    public static final String THIRTY = "Trente";
+    public static final String FORTY = "Quarante";
+
     private String player1Name;
     private String player2Name;
-    private int player1Score;
-    private int player2Score;
-
-    public void wonPoint(String playerName) {
-        if (playerName.equals("player1"))
-            player1Score += 1;
-        else
-            player2Score += 1;
-    }
+    private TennisScore player1Score;
+    private TennisScore player2Score;
+    private TennisGame tennisGame;
 
     private String globalScoreAsStringForEquality() {
         switch (player1Score) {
-            case 0:
-                return "Zéro partout";
-            case 1:
-                return "Quinze partout";
-            case 2:
-                return "Trente partout";
+            case ZERO:
+                return LOVE_ALL;
+            case FIFTEEN:
+                return FIFTEEN_ALL;
+            case THIRTY:
+                return THIRTY_ALL;
             default:
-                return "Egalité";
+                return DEUCE;
         }
     }
 
-    public String displayScore(String player1Name, int player1Score, String player2Name, int player2Score) {
-        this.player1Score = player1Score;
-        this.player2Score = player2Score;
+    public String displayScore(String player1Name, int player1ScoreNum, String player2Name, int player2ScoreNum) {
+        tennisGame = new TennisGame(player1ScoreNum, player2ScoreNum);
+        TennisScores tennisScores = tennisGame.score();
+        this.player1Score = tennisScores.player1;
+        this.player2Score = tennisScores.player2;
         if (player1Score == player2Score) {
             return globalScoreAsStringForEquality();
-        } else if (player1Score >= 4 || player2Score >= 4) {
+        } else if (tennisGame.endOfGame()) {
             return globalScoreAsStringForPointEnd();
         } else {
             return singlePlayerScoreAsString(player1Score) + "-" + singlePlayerScoreAsString(player2Score);
@@ -40,23 +47,22 @@ public class TennisFrenchDisplayer {
     }
 
     private String globalScoreAsStringForPointEnd() {
-        int scoreDiff = player1Score - player2Score;
-        if (scoreDiff == 1) return "Avantage player1";
-        else if (scoreDiff == -1) return "Avantage player2";
-        else if (scoreDiff >= 2) return "Jeu player1";
-        else return "Jeu player2";
+        if (player1Score == TennisScore.ADVANTAGE) return ADVANTAGE + "player1";
+        else if (player2Score == TennisScore.ADVANTAGE) return ADVANTAGE + "player2";
+        else if (player1Score == TennisScore.GAME) return WIN_FOR + "player1";
+        else return WIN_FOR + "player2";
     }
 
-    private String singlePlayerScoreAsString(int playerScore) {
+    private String singlePlayerScoreAsString(TennisScore playerScore) {
         switch (playerScore) {
-            case 0:
-                return "Zéro";
-            case 1:
-                return "Quinze";
-            case 2:
-                return "Trente";
+            case ZERO:
+                return LOVE;
+            case FIFTEEN:
+                return FIFTEEN;
+            case THIRTY:
+                return THIRTY;
             default:
-                return "Quarante";
+                return FORTY;
         }
     }
 

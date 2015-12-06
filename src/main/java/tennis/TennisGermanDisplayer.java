c33e670be0ebe4645a71a202d55a61dad2c4150e
Antoine MICHAUD <antoinemichaud34@gmail.com>
2015-12-06 17:35:26 +0100

    Refactor GermanDisplayer

--- src/main/java/tennis/TennisGermanDisplayer.java
+++ src/main/java/tennis/TennisGermanDisplayer.java
@@ -2,37 +2,44 @@ package tennis;
 
 public class TennisGermanDisplayer {
 
+    public static final String LOVE_ALL = "Null alles";
+    public static final String FIFTEEN_ALL = "Fünfzehn alles";
+    public static final String THIRTY_ALL = "Dreißig alles";
+    public static final String DEUCE = "Gleichstand";
+    public static final String ADVANTAGE = "Vorteil ";
+    public static final String WIN_FOR = "Spiel ";
+    public static final String LOVE = "Null";
+    public static final String FIFTEEN = "Fünfzehn";
+    public static final String THIRTY = "Dreißig";
+    public static final String FORTY = "Vierzig";
+
     private String player1Name;
     private String player2Name;
-    private int player1Score;
-    private int player2Score;
-
-    public void wonPoint(String playerName) {
-        if (playerName.equals("player1"))
-            player1Score += 1;
-        else
-            player2Score += 1;
-    }
+    private TennisScore player1Score;
+    private TennisScore player2Score;
+    private TennisGame tennisGame;
 
     private String globalScoreAsStringForEquality() {
         switch (player1Score) {
-            case 0:
-                return "Null alles";
-            case 1:
-                return "Fünfzehn alles";
-            case 2:
-                return "Dreißig alles";
+            case ZERO:
+                return LOVE_ALL;
+            case FIFTEEN:
+                return FIFTEEN_ALL;
+            case THIRTY:
+                return THIRTY_ALL;
             default:
-                return "Gleichstand";
+                return DEUCE;
         }
     }
 
-    public String displayScore(String player1Name, int player1Score, String player2Name, int player2Score) {
-        this.player1Score = player1Score;
-        this.player2Score = player2Score;
+    public String displayScore(String player1Name, int player1ScoreNum, String player2Name, int player2ScoreNum) {
+        tennisGame = new TennisGame(player1ScoreNum, player2ScoreNum);
+        TennisScores tennisScores = tennisGame.score();
+        this.player1Score = tennisScores.player1;
+        this.player2Score = tennisScores.player2;
         if (player1Score == player2Score) {
             return globalScoreAsStringForEquality();
-        } else if (player1Score >= 4 || player2Score >= 4) {
+        } else if (tennisGame.endOfGame()) {
             return globalScoreAsStringForPointEnd();
         } else {
             return singlePlayerScoreAsString(player1Score) + "-" + singlePlayerScoreAsString(player2Score);
@@ -40,23 +47,22 @@ public class TennisGermanDisplayer {
     }
 
     private String globalScoreAsStringForPointEnd() {
-        int scoreDiff = player1Score - player2Score;
-        if (scoreDiff == 1) return "Vorteil player1";
-        else if (scoreDiff == -1) return "Vorteil player2";
-        else if (scoreDiff >= 2) return "Spiel player1";
-        else return "Spiel player2";
+        if (player1Score == TennisScore.ADVANTAGE) return ADVANTAGE + "player1";
+        else if (player2Score == TennisScore.ADVANTAGE) return ADVANTAGE + "player2";
+        else if (player1Score == TennisScore.GAME) return WIN_FOR + "player1";
+        else return WIN_FOR + "player2";
     }
 
-    private String singlePlayerScoreAsString(int playerScore) {
+    private String singlePlayerScoreAsString(TennisScore playerScore) {
         switch (playerScore) {
-            case 0:
-                return "Null";
-            case 1:
-                return "Fünfzehn";
-            case 2:
-                return "Dreißig";
+            case ZERO:
+                return LOVE;
+            case FIFTEEN:
+                return FIFTEEN;
+            case THIRTY:
+                return THIRTY;
             default:
-                return "Vierzig";
+                return FORTY;
         }
     }
 

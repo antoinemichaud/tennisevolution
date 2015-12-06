41ba6fd1ac11f77375805f4804088c58fab66b4b
Marie-Laure Thuret <mthuret@xebia.fr>
2015-12-06 20:07:10 +0100

    scoring : proposal

--- server/routes.js
+++ server/routes.js
@@ -51,12 +51,11 @@ var rotatedRegisteredPlayers = {};
 
 
 var availablePoints = {
-  1: _.clone(stackPoints),
-  2: _.clone(stackPoints),
-  3: _.clone(stackPoints),
-  4: _.clone(stackPoints),
-  5: _.clone(stackPoints),
-  6: _.clone(stackPoints)
+  1: [20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+  2: [55, 34, 21, 13, 8, 5, 3, 2, 1],
+  3: [55, 34, 21, 13, 8, 5, 3, 2, 1],
+  4: [55, 34, 21, 13, 8, 5, 3, 2, 1],
+  5: [55, 34, 21, 13, 8, 5, 3, 2, 1]
 };
 
 var scoreBoard = {};
